be83d4379a755bfcd9f5d66df13dcb251d254fe8
Bhuwan-Upadhyay <bhuwanb898@gmail.com>
2024-10-25 14:02:55 +0530

    fix bugs

--- Backend/index.js
+++ Backend/index.js
@@ -5,48 +5,39 @@ import mongoose from "mongoose";
 import Form from "./model/Form.js";
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
-app.use(
-  cors({
-    origin: [
-      "https://portfolio-bcu001.vercel.app",
-      "https://portfolio-bcu001-jaoa.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
 app.use(bodyParser.json());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/PortfolioData");
+mongoose.connect("mongodb://localhost:27017/PortfolioData")
 
 app.get("/", (req, res) => {
-  res.send(`Backend of my Portfolio of `);
+  res.json({ message: "Welcome to Backend" });
 });
 
 app.post("/", async (req, res) => {
-  const { name, email, comment } = req.body;
-  const now = new Date();
-  const date = now.toString();
-
-  const newFormEntry = new Form({
-    name,
-    email,
-    comment,
-    date,
+    const { name, email, comment } = req.body;
+    const now = new Date();
+    const date = now.toString();
+  
+    const newFormEntry = new Form({
+      name,
+      email,
+      comment,
+      date,
+    });
+  
+    try {
+      await newFormEntry.save();
+      console.log("Form data saved to MongoDB");
+      res.status(200).send("Form data saved successfully!");
+    } catch (error) {
+      console.error("Error saving form data:", error);
+      res.status(500).send("Failed to save form data");
+    }
   });
 
-  try {
-    await newFormEntry.save();
-    console.log("Form data saved to MongoDB");
-    res.status(200).send("Form data saved successfully!");
-  } catch (error) {
-    console.error("Error saving form data:", error);
-    res.status(500).send("Failed to save form data");
-  }
-});
-
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Severing is running at http://localhost:${port}`);
 });
