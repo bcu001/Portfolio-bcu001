374c78fea80e9fdb85b7c848eb4cb5c9cb1c49ce
Bhuwan-Upadhyay <bhuwanb898@gmail.com>
2026-02-04 19:21:06 +0530

    feat:added icons

---
+++ frontend/lib/img/img.ts
@@ -0,0 +1,9 @@
+import github from "@/lib/img/github.svg";
+import X from "@/lib/img/X.svg";
+import linkedin from "@/lib/img/linkedin.svg";
+
+export const ICON = {
+  github,
+  X,
+  linkedin,
+};
